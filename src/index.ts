a8a053bbcae4e26bb418db0d807b5ad6eca45d49
Fei Chen <boxchenfei@gmail.com>
2023-04-22 23:38:19 +0200

    Restore the widget after refresh window

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import {
+  ILayoutRestorer,
   ILabShell,
   JupyterFrontEnd,
   JupyterFrontEndPlugin
@@ -9,6 +10,7 @@ import { requestAPI } from './handler';
 import { LoadLibrary } from 'visual-programming-editor2';
 import lib_example from './VPLibraryExample.json';
 import { VPDocWidget } from './widget';
+import { WidgetTracker } from '@jupyterlab/apputils';
 
 /**
  * Initialization data for the vp4jl extension.
@@ -17,10 +19,16 @@ const plugin: JupyterFrontEndPlugin<void> = {
   id: 'vp4jl:plugin',
   autoStart: true,
   requires: [ILabShell],
-  activate: (app: JupyterFrontEnd, labShell: ILabShell) => {
+  optional: [ILayoutRestorer],
+  activate: (
+    app: JupyterFrontEnd,
+    labShell: ILabShell,
+    restorer: ILayoutRestorer | null
+  ) => {
     console.log('JupyterLab extension vp4jl is activated!');
     const VP_FILE_TYPE = 'vp4jl';
     const VP_WIDGET_FACTORY = 'VP Editor';
+    const TRACKER_NAMESPACE = 'vp4jl';
 
     requestAPI<any>('get_example')
       .then(data => {
@@ -40,11 +48,32 @@ const plugin: JupyterFrontEndPlugin<void> = {
     // move to the server side
     LoadLibrary(lib_example);
 
+    const tracker = new WidgetTracker<VPDocWidget>({
+      namespace: TRACKER_NAMESPACE
+    });
+
+    if (restorer) {
+      restorer.restore(tracker, {
+        command: 'docmanager:open',
+        args: widget => ({
+          path: widget.context.path,
+          factory: VP_WIDGET_FACTORY
+        }),
+        name: widget => widget.context.path
+      });
+    }
+
     const widgetFactory = new VPWidgetFactory({
       name: VP_WIDGET_FACTORY,
       modelName: VP_MODEL_FACTORY,
       fileTypes: [VP_FILE_TYPE],
       defaultFor: [VP_FILE_TYPE]
+    });
+    widgetFactory.widgetCreated.connect((sender, widget) => {
+      widget.context.pathChanged.connect(() => {
+        tracker.save(widget);
+      });
+      tracker.add(widget);
     });
     app.docRegistry.addWidgetFactory(widgetFactory);
     app.docRegistry.addModelFactory(new VPModelFactory());
