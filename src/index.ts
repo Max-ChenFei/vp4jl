f620169430396c406e48f680401fe7a453ddd1dc
Fei Chen <boxchenfei@gmail.com>
2023-05-18 12:19:08 +0200

    Extract to function

--- src/index.ts
+++ src/index.ts
@@ -19,55 +19,12 @@ import { LoadPackageToRegistry } from 'visual-programming-editor';
 import { NodeExtension } from './node-extension';
 import { vp4jlIDs as gVP4jlIDs } from './namepace';
 
-/**
- * Initialization data for the vp4jl extension.
- */
 const vp4jl: JupyterFrontEndPlugin<void> = {
   id: 'vp4jl:plugin',
   autoStart: true,
   requires: [],
   optional: [ILayoutRestorer],
-  activate: (app: JupyterFrontEnd, restorer: ILayoutRestorer | null) => {
-    const vp4jlIDs = gVP4jlIDs;
-    // track and restore the widgets after reload
-    const tracker = new WidgetTracker<VPDocWidget>({
-      namespace: vp4jlIDs.trackerNamespace
-    });
-
-    if (restorer) {
-      restorer.restore(tracker, {
-        command: 'docmanager:open',
-        args: widget => ({
-          path: widget.context.path,
-          factory: vp4jlIDs.widgetFactory
-        }),
-        name: widget => widget.context.path
-      });
-    }
-
-    const widgetFactory = new VPWidgetFactory({
-      name: vp4jlIDs.widgetFactory,
-      modelName: vp4jlIDs.modelFactory,
-      fileTypes: [vp4jlIDs.fileType],
-      defaultFor: [vp4jlIDs.fileType]
-    });
-    widgetFactory.widgetCreated.connect((sender, widget) => {
-      widget.context.pathChanged.connect(() => {
-        tracker.save(widget);
-      });
-      tracker.add(widget);
-    });
-    app.docRegistry.addWidgetFactory(widgetFactory);
-    app.docRegistry.addModelFactory(new VPModelFactory());
-    app.docRegistry.addFileType({
-      name: vp4jlIDs.fileType,
-      displayName: 'VP File',
-      mimeTypes: ['text/json', 'application/json'],
-      extensions: [vp4jlIDs.fileExtension],
-      fileFormat: 'text',
-      contentType: 'file'
-    });
-  }
+  activate: activateVp4jl
 };
 
 const vp4jlMenu: JupyterFrontEndPlugin<void> = {
@@ -75,109 +32,14 @@ const vp4jlMenu: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [IDefaultFileBrowser, IFileBrowserFactory, IMainMenu],
   optional: [ILauncher, ICommandPalette],
-  activate: (
-    app: JupyterFrontEnd,
-    defaultFileBrowser: IDefaultFileBrowser,
-    browserFactory: IFileBrowserFactory,
-    mainMenu: IMainMenu,
-    launcher: ILauncher | null,
-    palette: ICommandPalette | null
-  ) => {
-    const vp4jlIDs = gVP4jlIDs;
-    app.commands.addCommand(vp4jlIDs.createNew, {
-      label: args =>
-        args['isPalette']
-          ? vp4jlIDs.createNewLabelInPalette
-          : args['isContextMenu']
-          ? vp4jlIDs.createNewLabelInContextMenu
-          : vp4jlIDs.createNewLabelInFileMenu,
-      caption: vp4jlIDs.caption,
-      execute: async args => {
-        const cwd =
-          args['cwd'] ||
-          browserFactory.tracker.currentWidget?.model.path ||
-          defaultFileBrowser.model.path;
-        const model = await app.commands.execute('docmanager:new-untitled', {
-          path: cwd,
-          contentType: 'file',
-          fileFormat: 'text',
-          ext: vp4jlIDs.fileExtension,
-          type: 'file'
-        });
-        return app.commands.execute('docmanager:open', {
-          path: model.path,
-          factory: vp4jlIDs.widgetFactory
-        });
-      }
-    });
-
-    mainMenu.fileMenu.newMenu.addGroup([{ command: vp4jlIDs.createNew }], 30);
-
-    launcher?.add({
-      command: vp4jlIDs.createNew,
-      category: vp4jlIDs.commandCategory,
-      rank: 0
-    });
-
-    palette?.addItem({
-      command: vp4jlIDs.createNew,
-      category: vp4jlIDs.commandCategory,
-      args: { isPalette: true }
-    });
-
-    app.contextMenu.addItem({
-      command: vp4jlIDs.createNew,
-      selector: '.jp-DirListing-content',
-      rank: 53,
-      args: {
-        isContextMenu: true
-      }
-    });
-  }
+  activate: activateVp4jlMenu
 };
 
 const vp4jlFixContextMenuClose: JupyterFrontEndPlugin<void> = {
   id: 'vp4jl:FixContextMenuClose',
   autoStart: true,
   requires: [ILabShell],
-  activate: (app: JupyterFrontEnd, labShell: ILabShell) => {
-    // close the context menu when switch the tab
-    labShell.currentChanged.connect((_, args) => {
-      if (args.oldValue instanceof VPDocWidget) {
-        args.oldValue.content.deactivate();
-      }
-      closeDefaultContextMenu();
-    });
-
-    function closeDefaultContextMenu() {
-      if (app.contextMenu.menu.isAttached) {
-        app.contextMenu.menu.close();
-      }
-    }
-
-    // close the context menu when click the tab
-    function addClickEventToSideBar() {
-      const sideBars = document.getElementsByClassName('jp-SideBar');
-      if (!sideBars.length) {
-        window.requestAnimationFrame(() => {
-          addClickEventToSideBar();
-        });
-        return;
-      }
-      for (const sideBar of sideBars) {
-        for (const tab of sideBar.getElementsByClassName('lm-TabBar-tab')) {
-          (tab as HTMLElement).addEventListener(
-            'click',
-            closeDefaultContextMenu
-          );
-        }
-      }
-    }
-
-    window.requestAnimationFrame(() => {
-      addClickEventToSideBar();
-    });
-  }
+  activate: activateVp4jlFixContextMenuClose
 };
 
 const vp4jlNodeExtension: JupyterFrontEndPlugin<void> = {
@@ -185,15 +47,7 @@ const vp4jlNodeExtension: JupyterFrontEndPlugin<void> = {
   autoStart: true,
   requires: [],
   optional: [ILayoutRestorer],
-  activate: (app: JupyterFrontEnd, restorer: ILayoutRestorer | null) => {
-    const nodeExtension = new NodeExtension();
-    app.shell.add(nodeExtension, 'left');
-
-    if (restorer) {
-      restorer.add(nodeExtension, 'vp4jlNodeExtension');
-    }
-    fetchNodeExtensions();
-  }
+  activate: activateVp4jlNodeExtension
 };
 
 const plugins: JupyterFrontEndPlugin<any>[] = [
@@ -203,6 +57,160 @@ const plugins: JupyterFrontEndPlugin<any>[] = [
   vp4jlNodeExtension
 ];
 export default plugins;
+
+function activateVp4jl(app: JupyterFrontEnd, restorer: ILayoutRestorer | null) {
+  const vp4jlIDs = gVP4jlIDs;
+  // track and restore the widgets after reload
+  const tracker = new WidgetTracker<VPDocWidget>({
+    namespace: vp4jlIDs.trackerNamespace
+  });
+
+  if (restorer) {
+    restorer.restore(tracker, {
+      command: 'docmanager:open',
+      args: widget => ({
+        path: widget.context.path,
+        factory: vp4jlIDs.widgetFactory
+      }),
+      name: widget => widget.context.path
+    });
+  }
+
+  const widgetFactory = new VPWidgetFactory({
+    name: vp4jlIDs.widgetFactory,
+    modelName: vp4jlIDs.modelFactory,
+    fileTypes: [vp4jlIDs.fileType],
+    defaultFor: [vp4jlIDs.fileType]
+  });
+  widgetFactory.widgetCreated.connect((sender, widget) => {
+    widget.context.pathChanged.connect(() => {
+      tracker.save(widget);
+    });
+    tracker.add(widget);
+  });
+  app.docRegistry.addWidgetFactory(widgetFactory);
+  app.docRegistry.addModelFactory(new VPModelFactory());
+  app.docRegistry.addFileType({
+    name: vp4jlIDs.fileType,
+    displayName: 'VP File',
+    mimeTypes: ['text/json', 'application/json'],
+    extensions: [vp4jlIDs.fileExtension],
+    fileFormat: 'text',
+    contentType: 'file'
+  });
+}
+
+function activateVp4jlMenu(
+  app: JupyterFrontEnd,
+  defaultFileBrowser: IDefaultFileBrowser,
+  browserFactory: IFileBrowserFactory,
+  mainMenu: IMainMenu,
+  launcher: ILauncher | null,
+  palette: ICommandPalette | null
+) {
+  const vp4jlIDs = gVP4jlIDs;
+  app.commands.addCommand(vp4jlIDs.createNew, {
+    label: args =>
+      args['isPalette']
+        ? vp4jlIDs.createNewLabelInPalette
+        : args['isContextMenu']
+        ? vp4jlIDs.createNewLabelInContextMenu
+        : vp4jlIDs.createNewLabelInFileMenu,
+    caption: vp4jlIDs.caption,
+    execute: async args => {
+      const cwd =
+        args['cwd'] ||
+        browserFactory.tracker.currentWidget?.model.path ||
+        defaultFileBrowser.model.path;
+      const model = await app.commands.execute('docmanager:new-untitled', {
+        path: cwd,
+        contentType: 'file',
+        fileFormat: 'text',
+        ext: vp4jlIDs.fileExtension,
+        type: 'file'
+      });
+      return app.commands.execute('docmanager:open', {
+        path: model.path,
+        factory: vp4jlIDs.widgetFactory
+      });
+    }
+  });
+
+  mainMenu.fileMenu.newMenu.addGroup([{ command: vp4jlIDs.createNew }], 30);
+
+  launcher?.add({
+    command: vp4jlIDs.createNew,
+    category: vp4jlIDs.commandCategory,
+    rank: 0
+  });
+
+  palette?.addItem({
+    command: vp4jlIDs.createNew,
+    category: vp4jlIDs.commandCategory,
+    args: { isPalette: true }
+  });
+
+  app.contextMenu.addItem({
+    command: vp4jlIDs.createNew,
+    selector: '.jp-DirListing-content',
+    rank: 53,
+    args: {
+      isContextMenu: true
+    }
+  });
+}
+
+function activateVp4jlFixContextMenuClose(
+  app: JupyterFrontEnd,
+  labShell: ILabShell
+) {
+  // close the context menu when switch the tab
+  labShell.currentChanged.connect((_, args) => {
+    if (args.oldValue instanceof VPDocWidget) {
+      args.oldValue.content.deactivate();
+    }
+    closeDefaultContextMenu();
+  });
+
+  function closeDefaultContextMenu() {
+    if (app.contextMenu.menu.isAttached) {
+      app.contextMenu.menu.close();
+    }
+  }
+
+  // close the context menu when click the tab
+  function addClickEventToSideBar() {
+    const sideBars = document.getElementsByClassName('jp-SideBar');
+    if (!sideBars.length) {
+      window.requestAnimationFrame(() => {
+        addClickEventToSideBar();
+      });
+      return;
+    }
+    for (const sideBar of sideBars) {
+      for (const tab of sideBar.getElementsByClassName('lm-TabBar-tab')) {
+        (tab as HTMLElement).addEventListener('click', closeDefaultContextMenu);
+      }
+    }
+  }
+
+  window.requestAnimationFrame(() => {
+    addClickEventToSideBar();
+  });
+}
+
+function activateVp4jlNodeExtension(
+  app: JupyterFrontEnd,
+  restorer: ILayoutRestorer | null
+) {
+  const nodeExtension = new NodeExtension();
+  app.shell.add(nodeExtension, 'left');
+
+  if (restorer) {
+    restorer.add(nodeExtension, 'vp4jlNodeExtension');
+  }
+  fetchNodeExtensions();
+}
 
 function fetchNodeExtensions() {
   requestAPI<any>('node_extension_manager')
